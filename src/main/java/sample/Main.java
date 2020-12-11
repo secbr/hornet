b6b6cf106fa932f22d87a282de64d0e55c57c8da
secbro <xinyoulingxi2008@126.com>
2020-12-11 14:54:50 +0800

    添加菜单

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,34 +1,64 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-//		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    @Override
+    public void start(Stage primaryStage) {
 
-		BorderPane root1 = new BorderPane();
-		root1.getStyleClass().add(getClass().getResource("sample.fxml").toExternalForm());
-		Label label = new Label("公众号：程序新视界");
-		root1.setCenter(label);
+        BorderPane root = new BorderPane();
+        root.getStyleClass().add(getClass().getResource("sample.fxml").toExternalForm());
+        Label label = new Label("公众号：程序新视界");
+        root.setCenter(label);
 
-		primaryStage.setTitle("Hornet");
-		Scene scene = new Scene(root1, 600, 550);
-		primaryStage.setScene(scene);
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(createAboutMenu());
+        root.setTop(menuBar);
 
-		primaryStage.show();
-	}
+        primaryStage.setTitle("Hornet");
+        Scene scene = new Scene(root, 600, 550);
+        primaryStage.setScene(scene);
 
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        primaryStage.show();
+    }
+
+    /**
+     * 关于菜单
+     */
+    private Menu createAboutMenu() {
+        Menu fileMenu = new Menu("About");
+        fileMenu.getItems().addAll(aboutAuthor(), exitMenuItem());
+        return fileMenu;
+    }
+
+    private MenuItem aboutAuthor() {
+        MenuItem author = new MenuItem("About Author");
+
+        return author;
+    }
+
+    /**
+     * 退出按钮
+     */
+    private MenuItem exitMenuItem() {
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setOnAction(actionEvent -> Platform.exit());
+        return exitMenuItem;
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
