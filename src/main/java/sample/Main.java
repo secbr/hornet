776dbd135dd7dfceeea04edd6e7c174acdfc9a58
secbro <xinyoulingxi2008@126.com>
2020-12-12 20:09:55 +0800

    添加协议选择

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,10 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -13,6 +15,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+
+	final String[] protocols = new String[]{"TCP", "UDP"};
+	final ChoiceBox<String> choiceBox = new ChoiceBox<>(FXCollections.observableArrayList(protocols));
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -27,17 +32,20 @@ public class Main extends Application {
 		hBox.setPadding(new Insets(15));
 		// 设置组件之间的间隙
 		hBox.setSpacing(15);
+		Label protocolLabel = new Label("协议：");
 		Label ipLabel = new Label("IP：");
 		TextField ipField = new TextField();
 		Label portLabel = new Label("端口：");
 		TextField portField = new TextField();
 		Button connect = new Button("连接");
-		hBox.getChildren().addAll(ipLabel, ipField, portLabel, portField, connect);
+		Button disconnect = new Button("端口");
+		hBox.getChildren().addAll(protocolLabel, choiceBox, ipLabel, ipField,
+				portLabel, portField, connect, disconnect);
 
 		root.setTop(hBox);
 
 		primaryStage.setTitle("Hornet");
-		Scene scene = new Scene(root, 600, 550);
+		Scene scene = new Scene(root, 800, 550);
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
