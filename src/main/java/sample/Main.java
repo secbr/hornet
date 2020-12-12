bcecfc8f5cf49822eae4b403352a4fed536c48c3
secbro <xinyoulingxi2008@126.com>
2020-12-12 19:58:10 +0800

    布局

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,76 +1,49 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
+	@Override
+	public void start(Stage primaryStage) {
 
-        BorderPane root = new BorderPane();
-        root.getStyleClass().add(getClass().getResource("sample.fxml").toExternalForm());
-        Label label = new Label("公众号：程序新视界");
-        label.setStyle("-fx-label-padding: 0 100px");
-        root.setBottom(label);
+		BorderPane root = new BorderPane();
+		root.getStyleClass().add(getClass().getResource("sample.fxml").toExternalForm());
 
-        Button button = new Button("连接");
-        button.setStyle("-fx-end-margin: 100px");
-        button.setOnAction(event -> {
-            String text = button.getText();
-            if ("连接".equals(text)) {
-                button.setText("断开");
-            } else {
-                button.setText("连接");
-            }
+		HBox hBox = new HBox();
+		// 设置布局位置
+		hBox.setAlignment(Pos.CENTER);
+		// 设备内部距离边框距离
+		hBox.setPadding(new Insets(15));
+		// 设置组件之间的间隙
+		hBox.setSpacing(15);
+		Label ipLabel = new Label("IP：");
+		TextField ipField = new TextField();
+		Label portLabel = new Label("端口：");
+		TextField portField = new TextField();
+		Button connect = new Button("连接");
+		hBox.getChildren().addAll(ipLabel, ipField, portLabel, portField, connect);
 
-        });
-        root.setCenter(button);
-//        root.getCenter().setStyle("");
+		root.setTop(hBox);
 
-        root.setRight(new Label("Right"));
-        root.setLeft(new Label("Left"));
+		primaryStage.setTitle("Hornet");
+		Scene scene = new Scene(root, 600, 550);
 
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(createAboutMenu());
-        root.setTop(menuBar);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
-        primaryStage.setTitle("Hornet");
-        Scene scene = new Scene(root, 600, 550);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * 关于菜单
-     */
-    private Menu createAboutMenu() {
-        Menu fileMenu = new Menu("About");
-        fileMenu.getItems().addAll(aboutAuthor(), exitMenuItem());
-        return fileMenu;
-    }
-
-    private MenuItem aboutAuthor() {
-        return new MenuItem("About Author");
-    }
-
-    /**
-     * 退出按钮
-     */
-    private MenuItem exitMenuItem() {
-        MenuItem exitMenuItem = new MenuItem("Exit");
-        exitMenuItem.setOnAction(actionEvent -> Platform.exit());
-        return exitMenuItem;
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
